e84761159a37ab1b7a3f5e9f11715b64a509f687
oooomilkywayoooo <105682588+oooomilkywayoooo@users.noreply.github.com>
2022-06-14 14:50:04 +0000

    [Add]varidatesの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -23,14 +28,18 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to "/books"
+    if book.destroy
+      flash[:notice] = "Book was successfully created."
+      redirect_to "/books"
+    end
   end
 
   private
